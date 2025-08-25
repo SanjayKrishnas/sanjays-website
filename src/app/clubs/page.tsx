b9c3e51133f5eb4d01a06bb36bc0c1264a98a17d
Your Name <sanjay.krishnas@gmail.com>
2025-08-25 15:42:05 -0700

    bug fixes

--- src/app/clubs/page.tsx
+++ src/app/clubs/page.tsx
@@ -2,42 +2,45 @@ import HeaderSection from "@/components/HeaderSection/HeaderSection"
 import ContentSection from "@/components/ContentSection/ContentSection"
 
 export default function Clubs() {
-    <div>
-        <HeaderSection
-            title="MY CLUBS"
-            subtitle="MY HOBBIES OUTSIDE OF SCHOOL"
-        />
 
-        <ContentSection
-            imageSrc=""
-            imageAlt=""
-            title="Project Lead @ BluePrint"
-            description={"Placeholder"}
-            backgroundColor="dark"
-        />
+    return (
+        <div>
+            <HeaderSection
+                title="MY CLUBS"
+                subtitle="MY HOBBIES OUTSIDE OF SCHOOL"
+            />
 
-        <ContentSection
-            imageSrc=""
-            imageAlt=""
-            title="Developer @ BluePrint"
-            description={"Placeholder"}
-            backgroundColor="light"
-        />
+            <ContentSection
+                imageSrc=""
+                imageAlt="blueprint_pl"
+                title="Project Lead @ BluePrint"
+                description={"Placeholder"}
+                backgroundColor="dark"
+            />
 
-        <ContentSection
-            imageSrc=""
-            imageAlt=""
-            title="Member @ ACM ICPC"
-            description={"Placeholder"}
-            backgroundColor="dark"
-        />
+            <ContentSection
+                imageSrc=""
+                imageAlt="blueprint_dev"
+                title="Developer @ BluePrint"
+                description={"Placeholder"}
+                backgroundColor="light"
+            />
 
-        <ContentSection
-            imageSrc=""
-            imageAlt=""
-            title="Member @ ACM Cyber"
-            description={"Placeholder"}
-            backgroundColor="light"
-        />
-    </div>
+            <ContentSection
+                imageSrc=""
+                imageAlt="icpc"
+                title="Member @ ACM ICPC"
+                description={"Placeholder"}
+                backgroundColor="dark"
+            />
+
+            <ContentSection
+                imageSrc=""
+                imageAlt="cyber"
+                title="Member @ ACM Cyber"
+                description={"Placeholder"}
+                backgroundColor="light"
+            />
+        </div>
+    );
 }
