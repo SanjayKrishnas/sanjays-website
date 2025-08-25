c990173f3600aa15e70b151d7c85de38bba86247
Your Name <sanjay.krishnas@gmail.com>
2025-08-25 15:20:16 -0700

    Projects iteration 1

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,51 @@
+import ContentSection from "@/components/ContentSection/ContentSection";
+import HeaderSection from "@/components/HeaderSection/HeaderSection"
 
 export default function Projects() {
-    
+    <div>
+        <HeaderSection
+            title="PROJECTS"
+            subtitle="MY PASSION PROJECTS!"
+        />
+
+        <ContentSection
+            imageSrc=""
+            imageAlt=""
+            title="Noteflow.ai"
+            description={"Placeholder"}
+            backgroundColor="light"
+        />
+
+        <ContentSection
+            imageSrc=""
+            imageAlt=""
+            title="EMG-to-QWERTY"
+            description={"Placeholder"}
+            backgroundColor="dark"
+        />
+
+        <ContentSection
+            imageSrc=""
+            imageAlt=""
+            title="RISC-V Multicore CPU"
+            description={"Placeholder"}
+            backgroundColor="light"
+        />
+
+        <ContentSection
+            imageSrc=""
+            imageAlt=""
+            title="BruinSquads"
+            description={"Placeholder"}
+            backgroundColor="dark"
+        />
+
+        <ContentSection
+            imageSrc=""
+            imageAlt=""
+            title="Mock Interview App"
+            description={"Placeholder"}
+            backgroundColor="light"
+        />
+    </div>
 }
