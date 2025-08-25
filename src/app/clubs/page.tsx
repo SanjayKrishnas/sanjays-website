0a09d1fda658caccd8be99cac8bb205298e89d61
Your Name <sanjay.krishnas@gmail.com>
2025-08-25 15:23:52 -0700

    Clubs Iteration 1

--- src/app/clubs/page.tsx
+++ src/app/clubs/page.tsx
@@ -1,4 +1,27 @@
+import HeaderSection from "@/components/HeaderSection/HeaderSection"
+import ContentSection from "@/components/ContentSection/ContentSection"
 
 export default function Clubs() {
-    
+    <div>
+        <HeaderSection
+            title="PROJECTS"
+            subtitle="MY PASSION PROJECTS!"
+        />
+
+        <ContentSection
+            imageSrc=""
+            imageAlt=""
+            title="Noteflow.ai"
+            description={"Placeholder"}
+            backgroundColor="light"
+        />
+
+        <ContentSection
+            imageSrc=""
+            imageAlt=""
+            title="EMG-to-QWERTY"
+            description={"Placeholder"}
+            backgroundColor="dark"
+        />
+    </div>
 }
