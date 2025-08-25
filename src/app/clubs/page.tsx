357b3f337d14ad2d2baeab8e6f515e4f05fd0f45
Your Name <sanjay.krishnas@gmail.com>
2025-08-25 15:26:03 -0700

    clubs iteration 2

--- src/app/clubs/page.tsx
+++ src/app/clubs/page.tsx
@@ -4,24 +4,24 @@ import ContentSection from "@/components/ContentSection/ContentSection"
 export default function Clubs() {
     <div>
         <HeaderSection
-            title="PROJECTS"
-            subtitle="MY PASSION PROJECTS!"
+            title="MY CLUBS"
+            subtitle="MY HOBBIES OUTSIDE OF SCHOOL"
         />
 
         <ContentSection
             imageSrc=""
             imageAlt=""
-            title="Noteflow.ai"
-            description={"Placeholder"}
-            backgroundColor="light"
-        />
-
-        <ContentSection
-            imageSrc=""
-            imageAlt=""
-            title="EMG-to-QWERTY"
+            title="Project Lead @ BluePrint"
             description={"Placeholder"}
             backgroundColor="dark"
+        />
+
+        <ContentSection
+            imageSrc=""
+            imageAlt=""
+            title="Developer @ BluePrint"
+            description={"Placeholder"}
+            backgroundColor="light"
         />
     </div>
 }
