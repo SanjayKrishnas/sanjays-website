68e72721799123fc57a11fc1251856fd0fa9fa04
Your Name <sanjay.krishnas@gmail.com>
2025-09-04 13:51:58 -0700

    Clubs iteration 2

--- src/app/clubs/page.tsx
+++ src/app/clubs/page.tsx
@@ -14,7 +14,14 @@ export default function Clubs() {
                 imageSrc=""
                 imageAlt="blueprint_pl"
                 title="Project Lead @ BluePrint"
-                description={"Placeholder"}
+                description={"Incoming Software DeveloperProject Lead for BluePrint 2026!"
+                    + "\n\n"
+                    + "Contributed to Blueprint's NPO recruitment as well as student recruitment." + 
+                    + "Also manage a team of 10 developers, following agile methodologies, to assign and keep track of sprints as well as ensure collaboration " +
+                    + "between our NPO partners and our team at Blueprint!"
+                    + "\n\n"
+                    + "More info coming soon!"
+                }
                 backgroundColor="dark"
             />
 
@@ -22,24 +29,24 @@ export default function Clubs() {
                 imageSrc=""
                 imageAlt="blueprint_dev"
                 title="Developer @ BluePrint"
-                description={"Placeholder"}
+                description={"Blueprint is a certificed 501 c3 non-profit organization that provides free software development services to non-profits and social good organizations."
+                    + "\n\n" 
+                    + "I participated in building Full Stack application primarly based in React, Node.js, and NoSQL databases, and worked alongside fellow developers and designers to create user friendly and functional applications." + 
+                    + "Blueprint strives to make technology accessible and useful for those who assist communities and promote public welfare."
+                }
                 backgroundColor="light"
-            />
+            />  
 
             <ContentSection
                 imageSrc=""
                 imageAlt="icpc"
                 title="Member @ ACM ICPC"
-                description={"Placeholder"}
+                description={"Took part as a member of ICPC's advanced coding track, where we practiced and expanded basic and advanced algorithmic tenchniques such as: "
+                    + "Binary search, DP, greedy, graph algorithms, and more!"
+                    + "\n\n" 
+                    + "Additionally, I participated in UCLA's school wide competitive programming competition and was able to place 2nd in the division."
+                }
                 backgroundColor="dark"
-            />
-
-            <ContentSection
-                imageSrc=""
-                imageAlt="cyber"
-                title="Member @ ACM Cyber"
-                description={"Placeholder"}
-                backgroundColor="light"
             />
         </div>
     );
